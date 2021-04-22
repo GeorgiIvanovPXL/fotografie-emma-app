c0568959cfafa323c258c9e1ca8e6f4ea7d16f88
GeorgiIvanovPXL <Georgi.Ivanov@student.pxl.be>
2021-04-22 15:03:37 +0200

    Fotografie WebApp
    Statische HTML, CSS en JS Fotografie website...

---
+++ script.js
@@ -0,0 +1,23 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+const aboutMoved = document.querySelector('.about');
+const row = document.querySelector('.row');
+
+
+
+showBtn.addEventListener('click', function() {
+    if (topNav.classList.contains('showNav')) {
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+        aboutMoved.classList.remove('navShown');
+        row.classList.remove('navShown')
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class="fas fa-times"></i>';
+        aboutMoved.classList.add('navShown');
+        row.classList.add('navShown')
+    }
+});
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
